6cbf3adfd98e55499ec8ad958b225ba7db3e449c
fksehf2 <fkstpdl2@gmail.com>
2024-07-25 17:53:19 +0900

    잔디...

--- src/pages/support/qna/EgovQnaDetail.jsx
+++ src/pages/support/qna/EgovQnaDetail.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 import { default as EgovLeftNav } from "components/leftmenu/EgovLeftNavSupport";
 
-function EgovQnaDetail({ num }) {
-  console.log("props num   " + num);
+function EgovQnaDetail({ props }) {
+  console.log("props num   " + props);
 
   return (
     <div className="container">
